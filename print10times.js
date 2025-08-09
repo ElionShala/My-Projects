a6a5eeb9d2b313dd42e4ef89fb92ed39fdcd606c
Elion <shalaelion1@gmail.com>
2025-08-09 10:55:57 +0200

    First Commit!

---
+++ print10times.js
@@ -0,0 +1,13 @@
+// Variables
+const total = 10;
+const text = "Hello World!";
+
+// Function
+function print() {
+    for (var i=1; i<=total; i++) {
+        console.log(`${i}. ${text}`)
+    }
+}
+
+// Function Calling
+print()
